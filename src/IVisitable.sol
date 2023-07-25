11fcbda0e5ef625db58e210e37e7f6297979948a
Lion's Habitat <lion@creaselabs.io>
2023-07-24 20:39:48 -0700

    Upgrading... broken WIP

--- src/IVisitable.sol
+++ src/IVisitable.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.17;
+pragma solidity ^0.8.21;
 
 interface IVisitable {
     function hasVisitor(address _visitor) external view returns (bool);
